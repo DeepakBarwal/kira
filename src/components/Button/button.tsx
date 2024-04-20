0d2046fb3f0531ba5d4cf6a3154f541ad963d2c7
Deepak Barwal <deepakbarwal98@gmail.com>
2024-04-20 19:20:52 +0530

    not evil

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,20 +1,11 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  onClick?: () => void;
+  onClick: () => void;
   children: ReactNode;
 }
 
 export const Button = ({ onClick, children }: ButtonProps) => {
-  const handler = () => {
-    console.log("heya");
-  };
-  if (!onClick)
-    return (
-      <button onClick={handler} data-testid="button-test">
-        {children}
-      </button>
-    );
   return (
     <button onClick={onClick} data-testid="button-test">
       {children}
